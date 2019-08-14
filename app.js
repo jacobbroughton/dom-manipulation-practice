f61958411f85b316064d1c529ea28f6859ff24e1
jlb1999 <JacobBroughton@JacobBroughtonsMacbook.local>
2019-08-14 11:59:45 -0400

    moved JS styles to a CSS sheet, cleaned up the JS code a bit

--- app.js
+++ app.js
@@ -1,87 +1,60 @@
 let columnOne = document.createElement("div");
 let columnTwo = document.createElement("div");
+columnOne.setAttribute("id","columnOne")
+columnTwo.setAttribute("id","columnTwo")
 
 document.body.appendChild(columnOne);
 document.body.appendChild(columnTwo);
 
-columnOne.style.height = "100%";
-columnOne.style.width = "50%";
-
-columnOne.style.border = "black solid 2px";
-columnOne.style.display = "flex";
-columnOne.style.flexDirection = "column";
-columnOne.style.justifyContent = "center";
-columnOne.style.alignItems = "center";
-columnTwo.style.height = "100%";
-columnTwo.style.width = "50%";
-columnTwo.style.border = "black solid 2px";
-columnTwo.style.display = "flex";
-columnTwo.style.flexDirection = "column";
-columnTwo.style.justifyContent = "center";
-columnTwo.style.alignItems = "center";
-
 
 // Use spans for smaller sections of code like text
 // Use divs for larger sections of code
-let clickElement = document.createElement("div");
+let clickMeElement = document.createElement("div");
 let clickHeading = document.createElement("h1");
-let element_style = clickElement.style;
-
-clickElement.setAttribute("id", "clickButton");
-
+clickMeElement.setAttribute("id", "clickMeElement");
 clickHeading.innerText = "Click Me";
-element_style.width = "50%";
-element_style.display = "flex";
-element_style.justifyContent = "center";
-element_style.border = "solid 2px black"
+clickMeElement.appendChild(clickHeading)
+columnOne.appendChild(clickMeElement)
 
-
-clickElement.appendChild(clickHeading)
-columnOne.appendChild(clickElement)
-
-clickElement.addEventListener("mousedown", function() {
-    element_style.boxShadow = "5px 5px black"
+clickMeElement.addEventListener("mousedown", function() {
+    clickMeElement.style.boxShadow = "5px 5px black"
 })
 
-clickElement.addEventListener("mouseup", function() {
-    element_style.boxShadow = "0px 0px black"
+clickMeElement.addEventListener("mouseup", function() {
+    clickMeElement.style.boxShadow = "0px 0px black"
 })
 // ----------------------------------------------
 
 let hoverElement = document.createElement("div");
 let hoverHeading = document.createElement("h1");
-let hoverElementStyle = hoverElement.style
-
 hoverElement.setAttribute("id", "hoverElement");
 hoverHeading.innerText = "Hover Me"
-
-hoverElement.style.marginTop = "5%"
-hoverElementStyle.border = "solid black 2px"
-hoverElementStyle.width = "50%";
-hoverElementStyle.display = "flex";
-hoverElementStyle.justifyContent = "center"
-
 hoverElement.appendChild(hoverHeading);
 columnOne.appendChild(hoverElement);
 
 hoverElement.addEventListener("mouseover", function() {
-    hoverElementStyle.boxShadow = "5px 5px black"
+    hoverElement.style.boxShadow = "5px 5px black"
 })
 
 hoverElement.addEventListener("mouseout", function() {
-    hoverElementStyle.boxShadow = "0px 0px black"
+    hoverElement.style.boxShadow = "0px 0px black"
 })
 
 // ---------------------------------------------
 let colorArray = ["green", "lightgreen", "lightgrey", "pink", "purple", "yellow", "brown", "blue", "teal"];
 let colorBoxArray = [];
-let elementsContainer = document.createElement("div");
+let mirrorElementsContainer = document.createElement("div");
 let initialElement = document.createElement("div");
 let mirroredElement = document.createElement("div");
 let initialHeading = document.createElement("h3");
 let mirroredHeading = document.createElement("h3");
 let colorBoxContainer = document.createElement("div");
 let colorBox;
+
+mirrorElementsContainer.setAttribute("id", "mirrorElementsContainer")
+initialElement.setAttribute("id", "initialElement")
+mirroredElement.setAttribute("id", "mirroredElement")
+colorBoxContainer.setAttribute("id", "colorBoxContainer")
 
 for(let i = 0; i < colorArray.length; i++) {
     colorBox = document.createElement("div");
@@ -98,35 +71,14 @@ for(let i = 0; i < colorArray.length; i++) {
     })
 }
 
-colorBoxContainer.style.height = "100%";
-colorBoxContainer.style.display = "flex"
-colorBoxContainer.style.justifyContent = "center"
-colorBoxContainer.style.flexDirection = "row"
-initialHeading.innerText = "Mirror me!";
-initialHeading.style.display = "flex";
-initialHeading.style.alignSelf = "center";
-mirroredHeading.innerText = "Mirrored!";
-mirroredHeading.style.alignSelf = "center";
-elementsContainer.style.border = "solid 2px black";
-elementsContainer.style.width = "50%";
-elementsContainer.style.height = "40%";
-elementsContainer.style.marginTop = "5%";
-initialElement.style.flexDirection = "column";
-initialElement.style.height = "50%";
-initialElement.style.paddingTop = "5%";
-initialElement.style.display = "flex";
-initialElement.style.justifyContent = "center";
-mirroredElement.style.height = "44%"
-mirroredElement.style.display = "flex";
-mirroredElement.style.justifyContent = "center";
-mirroredElement.style.borderTop = "solid 2px black";
 
+mirroredHeading.innerText = "Mirrored!"
 initialElement.appendChild(colorBoxContainer);
 initialElement.appendChild(initialHeading);
 mirroredElement.appendChild(mirroredHeading);
-elementsContainer.appendChild(initialElement);
-elementsContainer.appendChild(mirroredElement);
-columnOne.appendChild(elementsContainer);
+mirrorElementsContainer.appendChild(initialElement);
+mirrorElementsContainer.appendChild(mirroredElement);
+columnOne.appendChild(mirrorElementsContainer);
 
 mirroredElement.addEventListener("click", function() {
     mirroredElement.style.backgroundColor = initialElement.style.backgroundColor
@@ -139,10 +91,13 @@ let gridContainer = document.createElement("div")
 let amountSelectedDiv = document.createElement("div")
 let amountSelectedText = document.createElement("h3")
 let resetButton = document.createElement("button")
-columnTwo.appendChild(gridContainer)
 let unitArr = [];
 let selectedArr = []
 let amountSelected = 0;
+gridContainer.setAttribute("id", "gridContainer")
+amountSelectedDiv.setAttribute("id","amountSelectedDiv")
+columnTwo.appendChild(gridContainer)
+resetButton.setAttribute("id", "resetButton")
 
 for(let i = 0; i < gridNum; i++) {
     let gridUnit = document.createElement("div")
@@ -178,27 +133,17 @@ amountSelectedDiv.appendChild(resetButton)
 amountSelectedDiv.appendChild(amountSelectedText);
 columnTwo.appendChild(amountSelectedDiv);
 
-resetButton.style.height = "50%"
-resetButton.style.marginRight = "5%"
-amountSelectedDiv.style.display = "flex"
-amountSelectedDiv.style.flexDirection = "row"
-amountSelectedDiv.style.alignItems = "center"
-amountSelectedDiv.style.justifyContent = "center"
-amountSelectedDiv.style.width = "80%"
-gridContainer.style.height = "35%";
-gridContainer.style.width = "58%";
-gridContainer.style.display = "flex";
-gridContainer.style.justifyContent = "center";
-gridContainer.style.flexWrap = "wrap";
+
 
 
 // Grid that changes when you hover over certain parts
 let gridNum2 = 96;
 let gridContainer2 = document.createElement("div")
-columnTwo.appendChild(gridContainer2)
 let unitArr2 = [];
 let selectedArr2 = []
 let amountSelected2 = 0;
+gridContainer2.setAttribute("id", "gridContainer2")
+columnTwo.appendChild(gridContainer2)
 
 for(let i = 0; i < gridNum2; i++) {
     let dynamicGridUnit = document.createElement("div")
@@ -226,8 +171,6 @@ for(let i = 0; i < gridNum2; i++) {
     })
 }
 
-gridContainer2.style.height = "35%";
-gridContainer2.style.width = "58%";
-gridContainer2.style.display = "flex";
-gridContainer2.style.justifyContent = "center";
-gridContainer2.style.flexWrap = "wrap";
+
+// Make a doodle jump type of game
+// in here or separately
